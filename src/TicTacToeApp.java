1c08e8f6a2278b63ae41b1c8cd309d730ebb6bcf
DrCatnip <masterpanshul.164@gmail.com>
2026-04-20 09:06:09 +0530

    UC1: Display empty TicTacToe board

--- src/TicTacToeApp.java
+++ src/TicTacToeApp.java
@@ -1,3 +1,32 @@
-public class TicTacToeApp{
+public class TicTacToeApp {
 
+    // 3x3 board
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+
+        initializeBoard();
+        printBoard();
+    }
+
+    // Initialize board with '-'
+    static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    // Print board
+    static void printBoard() {
+        System.out.println("Tic-Tac-Toe Board:");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
